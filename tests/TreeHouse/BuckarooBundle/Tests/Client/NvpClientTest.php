eaf5320265c7c4d442ef66d2e8c636639c7c5c01
Randy Geraads <r.geraads@pararius.nl>
2020-09-04 10:32:44 +0200

    fixed tests

--- tests/TreeHouse/BuckarooBundle/Tests/Client/NvpClientTest.php
+++ tests/TreeHouse/BuckarooBundle/Tests/Client/NvpClientTest.php
@@ -5,8 +5,9 @@ namespace TreeHouse\BuckarooBundle\Tests\Client;
 use GuzzleHttp\Client;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\Psr7\Response;
-use Mockery as Mock;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Prophecy\Prophecy\ObjectProphecy;
 use TreeHouse\BuckarooBundle\Client\NvpClient;
 use TreeHouse\BuckarooBundle\Request\RequestInterface;
 use TreeHouse\BuckarooBundle\Response\ResponseInterface;
@@ -17,34 +18,32 @@ class NvpClientTest extends TestCase
     /**
      * @test
      */
-    public function it_does_not_decode_response_data()
+    public function it_can_send_a_nvp_request()
     {
-        $request = $this->getRequestMock([]);
-        $request->shouldReceive('getResponseClass')->once()->andReturn(MockResponse::class);
+        $data = ['fruit' => 'apple'];
 
-        $client = $this->createNvpClient(['foobar' => $encodedValue = 'foo%20bar@example.com']);
+        $request = $this->getRequestMock($data);
+        $request->getResponseClass()->willReturn(MockResponse::class);
 
-        /* @var MockResponse $response */
-        $response = $client->send($request);
-
-        $this->assertInstanceOf(ResponseInterface::class, $response);
-        $this->assertSame($encodedValue, $response->getFoobar());
+        $client = $this->createNvpClient();
+        $this->assertInstanceOf(ResponseInterface::class, $client->send($request->reveal()));
     }
 
     /**
      * @test
      */
-    public function it_can_send_a_nvp_request()
+    public function it_does_not_decode_response_data()
     {
-        $data = ['fruit' => 'apple'];
+        $request = $this->getRequestMock([]);
+        $request->getResponseClass()->willReturn(MockResponse::class);
 
-        $response = $this->getResponseMock();
+        $client = $this->createNvpClient(['foobar' => $encodedValue = 'foo%20bar@example.com']);
 
-        $request = $this->getRequestMock($data);
-        $request->shouldReceive('getResponseClass')->once()->andReturn($response);
+        /* @var MockResponse $response */
+        $response = $client->send($request->reveal());
 
-        $client = $this->createNvpClient();
-        $this->assertInstanceOf(ResponseInterface::class, $client->send($request));
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertSame($encodedValue, $response->getFoobar());
     }
 
     /**
@@ -54,34 +53,32 @@ class NvpClientTest extends TestCase
     {
         $client = $this->createNvpClient();
 
-        $response = $this->getResponseMock();
-
         $request = $this->getRequestMock([], 'transactionstatus');
-        $request->shouldReceive('getResponseClass')->once()->andReturn(get_class($response));
+        $request->getResponseClass()->willReturn(MockResponse::class);
 
         $getGatewayUrlMethod = new \ReflectionMethod(get_class($client), 'getGatewayUrl');
         $getGatewayUrlMethod->setAccessible(true);
-        $gatewayUrl = $getGatewayUrlMethod->invokeArgs($client, [$request, true]);
+        $gatewayUrl = $getGatewayUrlMethod->invokeArgs($client, [$request->reveal(), true]);
 
-        $client->send($request);
+        $client->send($request->reveal());
 
         $this->assertSame('https://testcheckout.buckaroo.nl/nvp/?op=transactionstatus', $gatewayUrl, 'The correct gateway URL was fetched.');
     }
 
     /**
      * @test
-     * @expectedException \RuntimeException
-     * @expectedExceptionMessage The response status code is not 200 (got 500)
      */
     public function it_can_send_a_nvp_request_which_returns_with_error()
     {
         $data = ['fruit' => 'apple'];
 
         $request = $this->getRequestMock($data);
-        $request->shouldNotReceive('getResponseClass');
+
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage('The response status code is not 200 (got 500)');
 
         $client = $this->createNvpClient([], 500);
-        $client->send($request);
+        $client->send($request->reveal());
     }
 
     /**
@@ -95,39 +92,29 @@ class NvpClientTest extends TestCase
         $websiteKey = 'website-key';
 
         /** @var SignatureGenerator $generator */
-        $generator = Mock::mock(SignatureGenerator::class);
-        $generator->shouldReceive('generate')->andReturn('1234');
+        $generator = $this->prophesize(SignatureGenerator::class);
+        $generator->generate(Argument::any())->willReturn('1234');
 
         $query = http_build_query(array_merge(['BRQ_SIGNATURE' => '1234'], $data));
 
         $guzzle = new Client(['handler' => new MockHandler([new Response($responseStatusCode, [], $query)])]);
-        $client = new NvpClient($guzzle, $generator, $websiteKey, true);
+        $client = new NvpClient($guzzle, $generator->reveal(), $websiteKey, true);
 
         return $client;
-    }
-
-    /**
-     * @return ResponseInterface
-     */
-    private function getResponseMock()
-    {
-        $response = Mock::mock(ResponseInterface::class);
-        $response->shouldReceive('create')->andReturn($response); // cant check arguments (not same instance)
-
-        return $response;
     }
 
     /**
      * @param array  $data
      * @param string $operation
      *
-     * @return RequestInterface
+     * @return ObjectProphecy|RequestInterface
      */
     private function getRequestMock(array $data = [], $operation = null)
     {
-        $request = Mock::mock(RequestInterface::class);
-        $request->shouldReceive('toArray')->once()->andReturn($data);
-        $request->shouldReceive('getOperation')->andReturn($operation);
+        /** @var RequestInterface $request */
+        $request = $this->prophesize(RequestInterface::class);
+        $request->toArray()->willReturn($data);
+        $request->getOperation()->willReturn($operation);
 
         return $request;
     }
